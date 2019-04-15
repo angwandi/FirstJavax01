5f19df446e636d0e650de47f701e6faba4fd647d
Innocent Angwadia <demsdema217@gmail.com>
2019-04-15 20:32:33 +0300

    Use code to load Gridpane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,32 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX!");
+        greeting.setTextFill(Color.GREEN);
+        greeting.setFont(Font.font("Roboto Slab", FontWeight.BOLD, 70));
+
+        root.getChildren().add(greeting);
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
