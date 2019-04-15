8eed93a9313dfa2b312c537c1aebcfe4a80ea7fc
Innocent Angwadia <demsdema217@gmail.com>
2019-04-15 20:55:57 +0300

    Layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,19 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        GridPane root = new GridPane();
-//////        root.setAlignment(Pos.CENTER);
-//////        root.setVgap(10);
-//////        root.setHgap(10);
-//////
-//////        Label greeting = new Label("Welcome to JavaFX!");
-//////        greeting.setTextFill(Color.GREEN);
-//////        greeting.setFont(Font.font("Roboto Slab", FontWeight.BOLD, 70));
-//////
-//////        root.getChildren().add(greeting);
 
         primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 700, 275));
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
