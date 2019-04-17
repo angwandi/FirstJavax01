0409c98971c65fc4164b178ef99e300585638108
Innocent Angwadia <demsdema217@gmail.com>
2019-04-17 20:58:23 +0300

    Add events on buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void onButtonClick(ActionEvent e) {
+
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello" + nameField.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButton);
+        byeButton.setDisable(disableButton);
+    }
+
+    @FXML
+    public void initialise() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
 }
